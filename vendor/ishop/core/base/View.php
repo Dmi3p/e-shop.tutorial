9ad8c9c128f7893ba314ff58069c394e4a9d8362
dpurshev <dpurshev94@gmail.com>
2018-11-28 17:55:29 +0300

    view.php finished

--- vendor/ishop/core/base/View.php
+++ vendor/ishop/core/base/View.php
@@ -1,18 +1,29 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace ishop\base;
 
-/**
- * Description of View
- *
- * @author dmitry
- */
 class View {
-    //put your code here
+    
+    public $route;
+    public $controller;
+    public $model;
+    public $view;
+    public $layout;
+    public $prefix;
+    public $data = [];
+    public $meta = [];
+    
+    public function __construct($route, $layuot = '', $view = '', $meta ) {
+        $this->route = $route;
+        $this->controller = $route['controller'];
+        $this->view = $view;
+        $this->prefix = $route['prefix'];
+        $this->model = $route['controller'];
+        $this->meta = $meta;
+        if($layuot === false){
+            $this->layout = false;
+        } else {
+            $this->layout = $layuot ?: LAYOUT;
+        }
+    }
 }
